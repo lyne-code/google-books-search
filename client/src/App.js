3f07696abf4e9210fd77f3a8b913ebb60a96b548
Lyne Malele <lynemalele@gmail.com>
2021-01-22 04:43:24 -0500

    code app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Save from "./pages/save";
-import Search from "./pages/search"
-import {BrowserRouter as Router, Route} from "react-router-dom"
-import Title from "./components/title"
-function App() {
+import React from "react";
+import Nav from "./components/Nav";
+import Books from "./pages/Books";
+import SavedBooks from "./pages/SavedBooks";
+import NoMatch from "./pages/NoMatch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => {
   return (
-     <Router>
-          <Route exact  path="/"  >
-               <Title name="Search" />
-               <Search/>
-             </Route>
-          <Route exact path="/save"  >
-          <Title name="Save" />
-               <Save/>
-             </Route>
-     </Router>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/savedbooks" component={SavedBooks} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
